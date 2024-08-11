f176f10940c283631ffd9f239d247aaf8b60af7d
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-11 19:26:26 +0200

    Copied over voxel type, minor changes, documentation written

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! Argentum - Voxel
+//! 
+//! `argentum_game_voxel` contains the voxel type used for Argentum.
+//! For more information about Argentum, see the `argentum_game` crate.
+
+type VoxelIdType = u8;
+
+/// The main building block of voxel worlds.
+/// Contains data on what kind of material the voxel should represent.
+#[derive(PartialEq, Debug, Default, Clone)]
+pub struct Voxel {
+    id: VoxelIdType
+}
+
+impl Voxel {
+    /// Creates a new Voxel
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// let voxel = argentum_game_voxel::Voxel::new(7);
+    /// 
+    /// ```
+    pub fn new(id: VoxelIdType) -> Self {
+        Voxel { id }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use quickcheck::quickcheck;
+
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    quickcheck! {
+        fn new_cube(id: VoxelIdType) -> bool {
+            let result = Voxel::new(id);
+            let expected = Voxel { id };
+            result == expected
+        }
     }
 }
