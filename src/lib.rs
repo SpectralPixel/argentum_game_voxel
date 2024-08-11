a1a554eba2a95d1fba2c81476fbbb61441c917bc
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-11 19:47:09 +0200

    Added getter for a Voxel's id, fixed up documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
-//! Argentum - Voxel
+//! # Argentum - Voxel
 //! 
 //! `argentum_game_voxel` contains the voxel type used for Argentum.
+//! 
 //! For more information about Argentum, see the `argentum_game` crate.
 
 type VoxelIdType = u8;
 
 /// The main building block of voxel worlds.
+/// 
 /// Contains data on what kind of material the voxel should represent.
 #[derive(PartialEq, Debug, Default, Clone)]
 pub struct Voxel {
@@ -18,12 +20,26 @@ impl Voxel {
     /// # Examples
     /// 
     /// ```
-    /// let voxel = argentum_game_voxel::Voxel::new(7);
-    /// 
+    /// use argentum_game_voxel::Voxel;
+    /// let voxel = Voxel::new(7);
     /// ```
     pub fn new(id: VoxelIdType) -> Self {
         Voxel { id }
     }
+
+    /// Return a Voxel's id
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use argentum_game_voxel::Voxel;
+    /// let voxel = Voxel::new(7);
+    /// let result = voxel.id();
+    /// assert_eq!(result, 7);
+    /// ```
+    pub fn id(&self) -> VoxelIdType {
+        self.id
+    }    
 }
 
 #[cfg(test)]
@@ -33,10 +49,18 @@ mod tests {
     use super::*;
 
     quickcheck! {
-        fn new_cube(id: VoxelIdType) -> bool {
+        fn new(id: VoxelIdType) -> bool {
             let result = Voxel::new(id);
             let expected = Voxel { id };
             result == expected
+        }
+    }
+
+    quickcheck! {
+        fn id(id: VoxelIdType) -> bool {
+            let voxel = Voxel::new(id);
+            let result = voxel.id();
+            result == id
         }
     }
 }
